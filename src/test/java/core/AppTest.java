b8a6f3bd6f724df44751888e1462e557f1e8f80f
Natalia Petrikovska <petrikovska@outlook.com>
2015-02-04 23:28:17 -0800

    Initial commit

--- src/test/java/core/AppTest.java
+++ src/test/java/core/AppTest.java
@@ -15,14 +15,14 @@ public class AppTest {
 
 public static void BeforeClass() throws Exception {
  
-System.out.println("BeforeClass method will be executed before first test method starts");
+//System.out.println("BeforeClass method will be executed before first test method starts");
 }
  
  @AfterClass
  
  public static void AfterClass() throws Exception {
  
-System.out.println("AfterClass method will be executed after last test method completed");
+//System.out.println("AfterClass method will be executed after last test method completed");
  
        }
  
@@ -30,7 +30,7 @@ System.out.println("AfterClass method will be executed after last test method co
  
  public void Before() throws Exception {
   
-       System.out.println("Before method will execute before every test method");
+       //System.out.println("Before method will execute before every test method");
  
        }
   
@@ -38,58 +38,51 @@ System.out.println("AfterClass method will be executed after last test method co
  
 public void After() throws Exception {
  
-       System.out.println("After method will execute after every test method");
+     //System.out.println("After method will execute after every test method");
         }
  
 @Test 
  
 public void Test_01_assertEquals_Positive() {
-    String s = "OK";
-    
-       System.out.println("Test_01_assertEquals_Positive:");
+	assertEquals("String not the same", "Testing JUnit 4", App.s);
        
-       assertEquals("failure - strings are not equal","OK",s);
-	
-  
+     //  System.out.println("Test_01_assertEquals_Positive:");
+             	
 }
   
 @Test
  
 public void Test_02_assertEquals_Negative() {
+	assertEquals("String not the same.", "Testing TestNG 6", App.s);
 	
-	String s = "OK";
-
-       System.out.println("Test_02_assertEquals_Negative");
-       
-       assertEquals("failure - strings are not equal","OK",s);
- 
+	    //   System.out.println("Test_02_assertEquals_Negative");
+               
        }
+ @Ignore
  
-@Ignore
- 
- 
-@Test
+ @Test
  
 public void Test_03_assertEquals_Ignored() {
+	 assertEquals("String not the same.", "Testing JUnit 3", App.s);
  
-
-       System.out.println("Test_03_assertEquals_Ignored");
+     //System.out.println("Test_03_assertEquals_Ignored");
  
        }
 
 @Test
 
 public void Test_04_AssertSame_Positive() {
+	assertSame("Integer not the same.", 55,App.i);
 
-       System.out.println("Test_04_AssertSame_Positive");
+      //System.out.println("Test_04_AssertSame_Positive");
 
        }
  
  @Test
  public void Test_05_AssertSame_Negative() {
-      System.out.println("Test_05_AssertSame_Negative");
+	 assertSame("Integer not the same.", 54,App.i);
+      //System.out.println("Test_05_AssertSame_Negative");
  
-
        }
  
 @Ignore
@@ -97,36 +90,38 @@ public void Test_04_AssertSame_Positive() {
 @Test
  
 public void Test_06_AssertSame_Ignored() {
+	assertSame("Integer not the same.", 56,App.i);
  
-       System.out.println("Test_06_AssertSame_Ignored");
+       //System.out.println("Test_06_AssertSame_Ignored");
         }
  
 @Test
 public void Test_07_assertFalse_Positive() {
-       System.out.println("Test_07_assertFalse_Positive");
+	assertFalse("Boolean should be false", App.f);
+       //System.out.println("Test_07_assertFalse_Positive");
        }
- 
-@Test
+ @Test
 
 public void Test_08_assertFalse_Negative() {
-       System.out.println("Test_08_assertFalse_Negative");
+	 assertFalse("Boolean should be false", App.t);
+      //System.out.println("Test_08_assertFalse_Negative");
        }
  
 @Test
  
 public void Test_09_assertTrue_Positive() {
+	assertTrue("Boolean should be true", App.t);
  
-       System.out.println("Test_09_assertTrue_Positive");
- 
-       }
+       //System.out.println("Test_09_assertTrue_Positive");
+        }
  
  
 @Test
 public void Test_10_assertTrue_Negative() {
+	assertTrue("Boolean should be true", App.f);
  
-       System.out.println("Test_10_assertTrue_Negative");
+       //System.out.println("Test_10_assertTrue_Negative");
  
-
        }
  
 }
